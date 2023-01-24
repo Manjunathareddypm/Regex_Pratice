9dec9adf388bec2b8061a1ef7a0d71ba34d07f99
Manjunatha Reddy PM <mreddypm@gmail.com>
2023-01-24 10:24:59 +0530

    UC1

--- src/com/regex/UserRegistration.java
+++ src/com/regex/UserRegistration.java
@@ -1,8 +1,18 @@
 package com.regex;
 
+import java.util.Scanner;
+import java.util.regex.Pattern;
+
 public class UserRegistration {
+    private static void checkValidFirstName() {
+        Scanner sc = new Scanner(System.in);
+        System.out.println("Enter your first name");
+        String firstName = sc.nextLine();
+        System.out.println(Pattern.matches("^[A-Z]{1}[a-z]{2,}$", firstName));
+    }
 
     public static void main(String[] args) {
         System.out.println("Welcome to Regex");
+        checkValidFirstName();
     }
 }
